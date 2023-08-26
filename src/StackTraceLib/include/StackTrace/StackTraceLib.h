98119a60105f3380422c21989aa4d46e3d5a485a
Oliver Knoll <starsky.hutch@gmx.ch>
2023-08-26 22:48:10 +0200

    Fix build on non-Windows platforms

--- src/StackTraceLib/include/StackTrace/StackTraceLib.h
+++ src/StackTraceLib/include/StackTrace/StackTraceLib.h
@@ -1,10 +1,14 @@
 #ifndef STACKTRACELIB_H
 #define STACKTRACELIB_H
 
+#ifdef PLATFORM_WINDOWS
 #ifdef STACKTRACE_EXPORT
-# define STACKTRACE_API __declspec(dllexport)
+  #define STACKTRACE_API __declspec(dllexport)
 #else
-# define STACKTRACE_API __declspec(dllimport)
+  #define STACKTRACE_API __declspec(dllimport)
+#endif
+#else
+  #define STACKTRACE_API
 #endif
 
 #endif // STACKTRACELIB_H
