d666a5544bdaf345d6cda0b5f32e9a9c3b901a12
Oliver Knoll <starsky.hutch@gmx.ch>
2023-09-21 19:07:53 +0200

    Update Cpptrace to v0.2.0-beta

--- src/StackTraceLib/src/StackTrace.cpp
+++ src/StackTraceLib/src/StackTrace.cpp
@@ -9,10 +9,10 @@
 void StackTrace::generate()
 {
     std::cout << "Stack frame addresses:" << std::endl;
-    const auto trace = cpptrace::generate_trace(0);
-    for(const auto &frame : trace) {
+    auto trace = cpptrace::generate_trace(0);
+    for (const auto &frame : trace) {
         std::cout << std::hex << frame.address << std::endl;
     }
     std::cout << "Cpptrace output:" << std::endl;
-    cpptrace::print_trace();
+    trace.print();
 }
